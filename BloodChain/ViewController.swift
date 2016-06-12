fb9fefddb11f2f84d42b67d772cbda309ba1ad75
drew0x9e <drew1patel@gmail.com>
2016-06-12 15:11:13 -0500

    Initial Commit

---
+++ BloodChain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BloodChain
+//
+//  Created by Drew Patel on 6/12/16.
+//  Copyright © 2016 Cloudgration. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
